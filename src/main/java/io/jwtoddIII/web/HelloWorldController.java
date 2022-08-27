aace437bf9644e0b1bb614f08640e290da985474
Trey Todd <treytodd@treys-mbp.lan>
2022-08-27 16:58:48 -0700

    first commit

---
+++ src/main/java/io/jwtoddIII/web/HelloWorldController.java
@@ -0,0 +1,13 @@
+package io.jwtoddIII.web;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(value = "/hello",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String hello() {
+        return "{'message':'hello'}";
+    }
+}
