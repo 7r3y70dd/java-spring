639091a19f7a83c7dae4b6e9f006b9cffdd7f794
Trey Todd <treytodd@treys-mbp.lan>
2022-12-28 20:55:05 -0800

    add post

--- src/main/java/io/jwtoddIII/web/HelloWorldController.java
+++ src/main/java/io/jwtoddIII/web/HelloWorldController.java
@@ -1,13 +1,18 @@
 package io.jwtoddiii.web;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping(value = "/hello",produces = MediaType.APPLICATION_JSON_VALUE)
-    public String hello() {
-        return "{'message':'hello'}";
+
+    @GetMapping(value = "/hello/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String hello(@PathVariable("id") String id) {
+        return "{'message':'" + id + "'}";
+    }
+
+    @PostMapping(value = "/hello/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String hello(@PathVariable("id") String id, @RequestBody String body) {
+        return "{'message':'" + id + "'}";
     }
 }
